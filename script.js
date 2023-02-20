b598a96935d145c0c412cb35cc39d22841dbdace
karolis steponavicius <karolis.stpn@gmail.com>
2023-02-20 21:03:04 +0200

    style from js to css

--- script.js
+++ script.js
@@ -14,15 +14,11 @@ fetch("./cars.json")
   .then((result) => {
     const output = document.getElementById("output");
 
-    document.getElementById("output").style.cssText =
-      "  margin-top: 50px; width: 90%; display: flex; flex-wrap: wrap; column-gap: 2%; row-gap: 20px;";
-
     result.forEach((car) => {
       const brandWrapper = document.createElement("div");
       brandWrapper.className = "card-wrapper";
+
       brandWrapper.innerHTML = "<H2>" + car.brand + "</H2>";
-      brandWrapper.style.cssText =
-        "width: 23.5%; padding: 50px; background-color: rgb(236, 236, 236); border-radius: 20px; box-shadow: rgba(51, 51, 51, 0.466) 0 0 10px;";
 
       const modelsWrapper = document.createElement("ul");
 
@@ -30,7 +26,6 @@ fetch("./cars.json")
         const modelItem = document.createElement("li");
         modelItem.innerText = model;
         modelsWrapper.appendChild(modelItem);
-        modelsWrapper.style.cssText = "margin-top: 20px";
       });
 
       brandWrapper.appendChild(modelsWrapper);
